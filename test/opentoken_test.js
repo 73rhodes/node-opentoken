38fa892ae20b5cc58009974077a4fee153f406ca
Darren DeRidder <drderidder@gmail.com>
2014-10-03 17:41:18 -0400

    Get HMAC validation working on decode.

--- test/opentoken_test.js
+++ test/opentoken_test.js
@@ -1,9 +1,15 @@
 var assert = require('assert');
 var otk = require('../opentoken.js');
 
+/**
+ * Test Case 1 (IETF Draft Interop Test 1)
+ * This test data in the IETF draft spec contains errors in the
+ * first character, making the header literal "PTK" instead of "OTK".
+ * This one has the proper 'OTK' header literal.
+ */
+var testToken = "T1RLAQK9THj0okLTUB663QrJFg5qA58IDhAb93ondvcx7sY6s44eszNqAAAga5W8Dc4XZwtsZ4qV3_lDI-Zn2_yadHHIhkGqNV5J9kw*"
 var testData = "foo=bar\nbar=baz";
-var testKey = "a66C9MvM8eY4qJKyCXKW+w==";
-var testToken = "UFRLAQK9THj0okLTUB663QrJFg5qA58IDhAb93ondvcx7sY6s44eszNqAAAga5W8Dc4XZwtsZ4qV3_lDI-Zn2_yadHHIhkGqNV5J9kw*"
+var testKey = "a66C9MvM8eY4qJKyCXKW+w=="; // from passwd generator function
 
 otk.decode(testToken, testKey, function (err, result) {
   process.stdout.write("Testing decode...");
@@ -12,9 +18,16 @@ otk.decode(testToken, testKey, function (err, result) {
   process.stdout.write("OK\n");
 });
 
-/*
-otk.encode(testData, testKey, null, function (err, result) {
-  console.log(err);
-  console.log(result);
+/**
+ * Test Case 2 (self generated from 3rd part OpenToken lib)
+ */
+var testToken2 = "T1RLAQLjjQ5X5syQ07anq_1m99BnDNTJexCNX35CAIIbj5A1kFp6vgn5AAAgyCcWB_xAGsUqiON2Sh4Yix5Ql8NV44MeWG4mbUKlRnE*";
+var testKey2 = "c2JvSUgMTn1OqAeAjT0wgA=="; // from passwd generator function
+var testData2 = "subject=foobar\nfoo=bar\nbar=baz";
+otk.decode(testToken2, testKey2, function (err, result) {
+  process.stdout.write("Testing decode 2...");
+  assert.ifError(err);
+  assert.equal(result.toString(), testData2);
+  process.stdout.write("OK\n");
 });
-*/
+
