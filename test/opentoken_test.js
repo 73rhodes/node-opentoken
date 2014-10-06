6de14ad647a2c7e48e7f43baafa65f36b96e37b2
Darren DeRidder <drderidder@gmail.com>
2014-10-06 14:31:04 -0400

    Clean up debugging code and test cases.

--- test/opentoken_test.js
+++ test/opentoken_test.js
@@ -10,9 +10,9 @@ var otk = require('../opentoken.js');
 var testToken = "T1RLAQK9THj0okLTUB663QrJFg5qA58IDhAb93ondvcx7sY6s44eszNqAAAga5W8Dc4XZwtsZ4qV3_lDI-Zn2_yadHHIhkGqNV5J9kw*"
 var testData = "foo=bar\nbar=baz";
 var testKey = "a66C9MvM8eY4qJKyCXKW+w=="; // from passwd generator function
-/*
+//*
 otk.decode(testToken, testKey, function (err, result) {
-  process.stdout.write("Testing decode...");
+  process.stdout.write("Test 1: decode...");
   assert.ifError(err);
   assert.equal(result.toString(), testData);
   process.stdout.write("OK\n");
@@ -26,38 +26,33 @@ otk.decode(testToken, testKey, function (err, result) {
 var testToken2 = "T1RLAQJHxWohC4euyRvd_Dfhmgj_F6jr5xD1QRzxbzWrTDX-SmPaE2dvAAAgTs4X_J3at_oDI4fStiIRX4S5WihrTpHY5ILYGUg7mxE*";
 var testKey2 = "c2JvSUgMTn1OqAeAjT0wgA=="; // from passwd generator function
 var testData2 = "subject=foobar\nfoo=bar\nbar=baz";
-//*
 otk.decode(testToken2, testKey2, function (err, result) {
-  process.stdout.write("Testing decode 2...");
+  process.stdout.write("Test 2: decode...");
   assert.ifError(err);
   assert.equal(result.toString(), testData2);
   process.stdout.write("OK\n");
 });
-// */
 
 /**
  * Test Case 3 (Encode)
  */
-/*
 var options = null;
 otk.encode(testData2, testKey2, options, function (err, result) {
-  process.stdout.write("Testing encode...");
+  process.stdout.write("Test 3: encode...");
   assert.ifError(err);
   assert.ok(result);
   process.stdout.write("OK\n");
+  console.log(result);
 });
-// */
 
 /**
  * Test Case 4 Encode & Decode
  */
-//*
 otk.encode(testData2, testKey2, null, function (err, token) {
+  process.stdout.write("Test 4: encode/decode...");
   assert.ifError(err);
-  console.log("encode: token = " + token);
   otk.decode(token, testKey2, function (err, data) {
-    console.log("Decoded token to " + data);
     assert.equal(data, testData2);
   });
+  process.stdout.write("OK\n");
 });
-// */
